fb97dbdf32e88ca374820f823b35d06aab875409
Anika Bansal <anikabansal@Anikas-MacBook-Air.local>
2025-02-24 13:22:31 -0600

    deleted files

--- AskDocsAI.py
+++
@@ -1,9 +0,0 @@
-from together import Together
-
-client = Together()
-
-response = client.chat.completions.create(
-    model="meta-llama/Llama-3.3-70B-Instruct-Turbo",
-    messages=[{"role": "user", "content": "What are some fun things to do in New York?"}],
-)
-print(response.choices[0].message.content)
